dde29d05d1758752879366a158737c88c2f83d80
Rasheed <65327262+rasheedrid@users.noreply.github.com>
2024-01-18 15:03:07 +0700

    Add files via upload

---
+++ pin-blynk.h
@@ -0,0 +1,23 @@
+/*
+   Pengaturan Pin Virtual di Aplikasi Blynk Legacy
+*/
+#define vPIN_PV_POWER1                   V1
+#define vPIN_PV_CURRENT1                 V2
+#define vPIN_PV_VOLTAGE1                 V3
+#define vPIN_BATTERY_CHARGE_POWER1       V4
+#define vPIN_BATTERY_CHARGE_CURRENT1     V5
+#define vPIN_BATT_VOLTAGE1               V6
+#define vPIN_LOAD_POWER1                 V7
+#define vPIN_LOAD_CURRENT1               V8
+#define vPIN_BATT_REMAIN1                V9
+#define vPIN_BATT_TEMP1                  V10
+#define vPIN_CONTROLLER_TEMP1            V11
+#define vPIN_LOAD_ENABLED1               V12
+#define vPIN_DAILY_ENERGY_GNERATED1      V13
+#define vPIN_GENERATED_ENERGY_MONTH1     V14
+#define vPIN_GENERATED_ENERGY_YEAR1      V15
+#define vPIN_CONSUMED_ENERGY_TODAY1      V16
+#define vPIN_CONSUMED_ENERGY_MONTH1      V17
+#define vPIN_CONSUMED_ENERGY_YEAR1       V18
+#define vPIN_CARBON_REDUCTION1           V19
+#define vPIN_LOAD_VOLTAGE_1              V20
